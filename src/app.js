4cd6d09790a83f1aea51285277a9429be4432f65
ganesh <goudaganesh84@gmail.com>
2026-01-24 22:10:32 +0530

    feat: user registration and login with JWT authentication

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const cors = require('cors')
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRoutes);
+
 
 app.get("/health",(req,res)=>{
     res.send(200).json({status : "OK" , message :"server is running" })
